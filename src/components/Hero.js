27f1d95139fd839053517998fe7a7cb93109e924
jacobnathanson <jacobnath93@gmail.com>
2022-03-30 14:53:50 -0400

    medium query and changed to svg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,10 +7,10 @@ export default function Hero() {
                 <h1>Hey there, I’m
                     Jacob Nathanson
                 </h1>
-                <h3>
+                <h2>
                     Full stack developer  from
                     Brooklyn, New York
-                </h3>
+                </h2>
                 <div className="hero-icon-container">
                     <div className="mail-icon" />
                     <div className="github-icon" />
