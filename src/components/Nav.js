20afb36674720b7d4fbebf344155219546822a01
jacobnathanson <jacobnath93@gmail.com>
2022-03-31 19:27:14 -0400

    resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ export default function Nav() {
                 <a href="#contact">contact</a>
                 <a href="#about">about</a>
                 <a href="#projects">projects</a>
+                <a href="resume/resume.pdf" download>resume</a>
             </div>
         </nav>
     )
