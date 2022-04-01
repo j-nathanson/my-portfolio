28fb320f0458429baaf2cf22474793a393b262ec
jacobnathanson <jacobnath93@gmail.com>
2022-04-01 13:20:24 -0400

    switched to using react-scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 export default function Nav() {
     return (
         <nav>
             <div className="nav-links">
-                <a href="#contact">contact</a>
-                <a href="#about">about</a>
-                <a href="#projects">projects</a>
+                <Link
+                    activeClass='active'
+                    to='contact'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >contact</Link>
+                <Link
+                    activeClass='active'
+                    to='about'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >about</Link>
+                <Link
+                    activeClass='active'
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >projects</Link>
                 <a href="resume/resume.pdf" download>resume</a>
             </div>
         </nav>
