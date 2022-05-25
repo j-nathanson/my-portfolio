9cdc0f4f99018ffb1dbe388ffd4ec00f632eea8c
jacobnathanson <jacobnath93@gmail.com>
2022-05-25 13:20:38 -0400

    added descriptify product

--- src/shared/project-data.js
+++ src/shared/project-data.js
@@ -2,6 +2,7 @@ import dreamy from '../assets/images/dreamy.png'
 import banhmi from '../assets/images/banh-mi-mama.png'
 import foodies from '../assets/images/foodies.png'
 import portfolio from '../assets/images/portfolio.png'
+import descripify from '../assets/images/descriptify.png'
 
 export const PROJECT_DATA = [
     {
@@ -12,18 +13,25 @@ export const PROJECT_DATA = [
         liveUrl: 'https://dreamy-journal.netlify.app/'
     },
     {
-        title: 'Banh mi mama - a food ordering app',
-        img: banhmi,
-        description: 'React Native - Redux Toolkit - React Hook Form',
-        gitUrl: 'https://github.com/j-nathanson/banh-mi-mama',
-        liveUrl: ''
-    },
-    {
         title: 'NYCFT - a food truck catering app',
         img: foodies,
         description: 'React  - Redux Toolkit - Bootstrap',
         gitUrl: 'https://github.com/j-nathanson/foodtrucks',
         liveUrl: 'https://nycft.herokuapp.com/home',
+    },
+    {
+        title: 'Descriptify - product describer',
+        img: descripify,
+        description: 'React  - Redux Toolkit - Bootstrap',
+        gitUrl: 'https://github.com/j-nathanson/openai-descriptify',
+        liveUrl: 'https://descriptify.netlify.app/',
+    },
+    {
+        title: 'Banh mi mama - a food ordering app',
+        img: banhmi,
+        description: 'React Native - Redux Toolkit - React Hook Form',
+        gitUrl: 'https://github.com/j-nathanson/banh-mi-mama',
+        liveUrl: ''
     },
     {
         title: 'My Portfolio page',
